82eb8a2d31ed43b5bc521a627f5697fa72acbb55
Alex Sarco <ale@sarco.com.ar>
2020-12-18 18:36:37 +0000

    Hashtag configurable by env variable

--- src/main/java/com/imgarena/TechWeek.java
+++ src/main/java/com/imgarena/TechWeek.java
@@ -1,8 +1,13 @@
 package com.imgarena;
 
+import com.amazonaws.util.StringUtils;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import twitter4j.*;
 
 public class TechWeek {
+
+    private static final Logger LOGGER = LogManager.getLogger(TechWeek.class);
 
     public static void main(String[] args) {
 
@@ -16,8 +21,15 @@ public class TechWeek {
 
         var ivsService = new IVSService();
 
+        var hashtag = System.getenv("twitter.hashtag");
+        if (StringUtils.isNullOrEmpty(hashtag)) {
+            hashtag = "#ufc";
+        }
+        LOGGER.info("Hashtag set to: {}", hashtag);
+
+
         twitterStream.addListener(new TechWeekListener(twitter, ivsService));
-        FilterQuery filterQuery = new FilterQuery().language("en").track("#ufc");
+        FilterQuery filterQuery = new FilterQuery().language("en").track(hashtag);
         twitterStream.filter(filterQuery);
     }
 }
